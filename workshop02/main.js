fdfd83fbf712d23b824defa29cf971f25ed2f993
Tan Shin Yee Janie (IFAP IT DSA B2B) <xiny80@gmail.com>
2019-08-21 17:13:51 +0800

    workshop2

--- workshop02/main.js
+++ workshop02/main.js
@@ -13,8 +13,8 @@ console.info(`Using ${keys.mongo}`);
 
 const db = CitiesDB({  
 	connectionUrl: keys.mongo, 
-	databaseName: 'zips', 
-	collectionName: 'city'
+	databaseName: 'cities', 
+	collectionName: 'cities'
 });
 
 const app = express();
@@ -26,17 +26,99 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
+app.get(
+	'/api/states', 
+	(req, resp)=>{
+		db.findAllStates()
+			.then(result => {
+				resp.status(200);
+				resp.type('application/json');
+				resp.json(
+					result.map(v=> `/api/state/${v}`)
+					);
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			})
+	}
+)
 
 
 
+// TODO HEAD /api/state/:state
+app.head(
+	'api/state/:state',
+	(req, resp) => {
+		//should check if state exist
+		resp.status(200);
+		resp.type('application/json');
+		resp.header('Accept-Ranges','items');
+		resp.end();
+	}
+)
 
 // TODO GET /api/state/:state
-
+app.get(
+	'/api/state/:state',
+	range({ accept:'cities', limit:20 }),
+	(req, resp)=> {
+		const state = req.params.state;
+		const first = req.range.first;
+		const last = req.range.last;
+		Promise.all([
+			db.findCitiesByState(state, {limit: (last - first + 1), offset:first }),
+			db.countCitiesInState(state)
+		
+		]).then(results => {
+				resp.status(206);
+				resp.type('application/json');
+				resp.range({
+					first:first,
+					last: last,
+					length: results[1]
+				})
+				resp.json(results[0].map(v=> `/api/city/${v}`)
+					);
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			})
+	}
+)
 
 
 
 // TODO GET /api/city/:cityId
+app.get(
+	'/api/city/:cityId',
+	(req, resp) => {
+		const cityId = req.params.cityId;
+		db.findCityById(cityId)
+			.then(result => {
+				resp.type('application/json');
+					
+				if(result.length > 0) {	
+					resp.status(200);
+					resp.json(result[0]);
+				} 
+				else{
+					resp.status(404);
+					resp.send({message:`CityId ${cityId} not found`});
+				}
+				
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			})
+	}
 
+)
 
 
 // TODO POST /api/city
