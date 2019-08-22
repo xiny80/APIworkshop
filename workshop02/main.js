eb79d5c3c246b0dc8df42e75d50dd1abd8fe0884
Tan Shin Yee Janie (IFAP IT DSA B2B) <xiny80@gmail.com>
2019-08-22 14:46:16 +0800

    workshop2-post-to-city

--- workshop02/main.js
+++ workshop02/main.js
@@ -123,7 +123,25 @@ app.get(
 
 // TODO POST /api/city
 
+app.post(
+	'/api/city',
+	(req, resp) => {
+		const data = req.body
+		console.info('>>data: ', data)
 
+		db.insertCity(data)
+			.then(result => {
+				resp.status(201)
+				resp.type('application/json')
+				resp.json({message: 'added'})
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			})
+	}
+)
 
 
 // Optional workshop
